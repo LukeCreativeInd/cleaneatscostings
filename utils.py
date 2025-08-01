89445f8ceaaa89a8f05987644ad772c3e9cabbfe
LukeCreativeInd <71154774+LukeCreativeInd@users.noreply.github.com>
2025-08-01 14:23:03 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
-# utils.py
-
 import os
 import pandas as pd
 
 def save_ingredients(df: pd.DataFrame):
+    os.makedirs("data", exist_ok=True)  # ✅ ensures folder exists
     df.to_csv("data/ingredients.csv", index=False)
